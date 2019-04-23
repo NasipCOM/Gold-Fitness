2ef4b306baf9498f64de410f74b413201bec0cb6
NasipCOM <salmahunov@mail.ru>
2019-04-23 19:00:23 +0600

    Fixed bags with responsibility

--- assets/js/script.js
+++ assets/js/script.js
@@ -373,11 +373,39 @@ $(document).ready(function() {
     dots: true,
     slidesToShow: 5,
     slidesToScroll: 5,
-    adaptiveHeight: true
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 });
 
 // This function return a full format a phtoto when u click
-function imgWindow(){
-  window.open("image")
+function imgWindow() {
+  window.open("image");
 }

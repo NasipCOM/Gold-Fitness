63306c04a0e15890e63ab8a3d26d131b2f4c31b8
NasipCOM <salmahunov@mail.ru>
2019-05-16 09:29:26 +0600

    GOld

--- assets/js/script.js
+++ assets/js/script.js
@@ -411,5 +411,24 @@ function imgWindow() {
 }
 
 
-//botton
+// Get the modal
+var modal = document.querySelector('.myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.querySelector('.myImg');
+var modalImg = document.querySelector(".img1");
+var captionText = document.querySelector("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
 

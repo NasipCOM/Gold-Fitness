647c5043067636c3818b7588170d12cb531d8002
NasipCOM <salmahunov@mail.ru>
2019-05-17 11:53:11 +0600

    Added a new shop

--- assets/js/script.js
+++ assets/js/script.js
@@ -404,34 +404,3 @@ $(document).ready(function() {
     ]
   });
 });
-
-// This function return a full format a phtoto when u click
-/* function imgWindow() {
-  window.open("image");
-}
-
-
-// Get the modal
-var modal = document.querySelector('.myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.querySelector('.myImg');
-var modalImg = document.querySelector(".img1");
-var captionText = document.querySelector("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
-
-$(document).ready(function() {
-  $('.myImg').magnificPopup({type:'image'});
-}); */

7a10707b3f207ed9870de5825f21ff6a6b2934d3
NasipCOM <salmahunov@mail.ru>
2019-12-28 22:52:06 +0600

    gulp min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,18 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+// var sass = require('gulp-sass');
+var uglifycss = require('gulp-uglifycss');
+
+// gulp.task('scss', function () {
+//     return gulp.src('./assets/scss/*.scss')
+//       .pipe(sass().on('error', sass.logError))
+//       .pipe(gulp.dest('./css1'));
+//   });
 
 
+  gulp.task('css', function () {
+  gulp.src('./assets/css/style.css')
+    .pipe(uglifycss({
+      "uglyComments": true
+    }))
+    .pipe(gulp.dest('./assets/mincss/'));
+});

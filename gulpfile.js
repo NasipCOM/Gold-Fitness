4d87ff73dea2ccdcf8ecfc6700128520c15bdd5c
NasipCOM <salmahunov@mail.ru>
2019-12-29 21:53:45 +0600

    gulp minification images

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,27 @@ var gulp = require('gulp');
 var uglifycss = require('gulp-uglifycss');
 const imagemin = require('gulp-imagemin');
 
-gulp.task('img', function () {
-	gulp.src('assets/images1/*')
+gulp.task('img', async function () {
+	gulp.src("assets/images_original/**/*")
 		.pipe(imagemin())
-		.pipe(gulp.dest('assets/imgages'))
+		.pipe(gulp.dest('assets/images'))
 });
-gulp.task('imgage', function () {
-	gulp.src('assets/images1/*/*')
+gulp.task('image', async function () {
+	gulp.src("assets/images_original/*")
 		.pipe(imagemin())
-		.pipe(gulp.dest('assets/imgages'))
+		.pipe(gulp.dest('assets/images'))
 });
 
-
-
-  gulp.task('css', function () {
+gulp.task('css', async function () {
   gulp.src('./assets/css/style.css')
     .pipe(uglifycss({
       "uglyComments": true
     }))
     .pipe(gulp.dest('./assets/mincss/'));
 });
+
+// gulp.task('watch',async function(){
+//   gulp.watch('./assets/css/style.css', 'css');
+//   gulp.watch('./assets/images_original', 'img');
+// });
+gulp.task('default', gulp.parallel('css', 'img', 'image'));
